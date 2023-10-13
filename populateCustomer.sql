180be215af15a1d63563f6f40f9af1834a507fa2
Vivek Pratap <vivprata@in.ibm.com>
2023-10-13 13:13:14 +0530

    Create populateCustomer.sql
    First commit of table values

---
+++ populateCustomer.sql
@@ -0,0 +1,29 @@
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1001', 'John Doe', 'Active', 'Preference A', 'Bachelor');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES ('A1002', 'Jane Smith', 'Inactive', 'Preference B', 'Master');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1003', 'Michael Johnson', 'Active', 'Preference C', 'Doctorate');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1004', 'Emily Brown', 'Active', 'Preference A', 'Bachelor');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1005', 'William Lee', 'Inactive', 'Preference B', 'Master');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1006', 'Olivia Wilson', 'Active', 'Preference C', 'Doctorate');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1007', 'James Miller', 'Active', 'Preference A', 'Bachelor');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1008', 'Sophia Davis', 'Inactive', 'Preference B', 'Master');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1009', 'Benjamin Wilson', 'Active', 'Preference C', 'Doctorate');
+
+INSERT INTO db2_catalog.retailbank.Customer ("Customer_Id", "Customer_name", "Customer_status", "Customer_preferences", "Customer_education")
+VALUES('A1010', 'Isabella Martinez', 'Active', 'Preference A', 'Bachelor');
